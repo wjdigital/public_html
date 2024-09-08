09b1912c636fb92b25eb1ece50c12e2fbaa379ab
wjdigital <45724694+wjdigital@users.noreply.github.com>
2024-09-08 00:30:03 -0300

    pwa fortune tiger

---
+++ pt/cassino/games/fortune-tiger/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'slotezybet-cache-v1';
+const urlsToCache = [
+  'https://slotezybet.com/pt/cassino/games/fortune-tiger/',
+  'https://slotezybet.com/pt/cassino/games/fortune-tiger/index.html',
+  'https://slotezybet.com/pt/cassino/games/fortune-tiger/assets/img/pwa/icon1-192x192.png',
+  'https://slotezybet.com/pt/cassino/games/fortune-tiger/assets/img/pwa/icon1-512x512.png',
+  // Adicione outros recursos aqui que precisam ser cacheados
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
